55c34410f596ccb0f43e414b3600a42bc640960a
The Hutchins Center at Brookings <60798139+hutchinsra@users.noreply.github.com>
2020-10-05 09:57:37 -0400

    Created web scraping script

---
+++ code/scrape-secrets.R
@@ -0,0 +1,36 @@
+## ---------------------------
+##
+## Script name: 
+##
+## Purpose of script:
+##
+## Author: Manuel Alcalá Kovalski
+##
+## Date Created: 2020-10-05
+##
+## Email: malcalakovalski@brookings.edu
+##
+## ---------------------------
+##
+## Notes:
+##   
+##
+## ---------------------------
+
+options(scipen = 6, digits = 4) # I prefer to view outputs in non-scientific notation
+memory.limit(30000000)     # this is needed on some PCs to increase memory allowance, but has no impact on macs.
+
+## ---------------------------
+
+## load up the packages we will need:  (uncomment as required)
+
+library('tidyverse')
+# source("functions/packages.R")       # loads up all the packages we need
+
+## ---------------------------
+
+## load up our functions into memory
+
+# source("functions/summarise_data.R") 
+
+## ---------------------------
